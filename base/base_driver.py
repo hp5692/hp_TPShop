4f238b63d6d506fd1751aec4e6c8e47259723cf0
hp5692 <hanpeng5692@163.com>
2018-11-15 15:37:54 +0800

    初始化项目

---
+++ base/base_driver.py
@@ -0,0 +1,14 @@
+from appium import webdriver
+
+
+def init_driver():
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1'
+    desired_caps['deviceName'] = '192.168.56.101:5555'
+    desired_caps['appPackage'] = 'com.android.browser'
+    desired_caps['appActivity'] = '.BrowserActivity'
+    desired_caps['noReset'] = True
+    # 声明driver对象
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+    return driver
